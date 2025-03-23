035bb89f038966fcb90c9a92d12fb5291c226e98
Milgrau06 <danilosouza0860@gmail.com>
2025-03-23 15:28:43 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,15 @@
+public class Tabuada {
+    public static void main (String[] args) {
+        int numMulti = 9;
+
+    System.out.println("\n=================================");
+    System.out.println("Tabuada de multiplicação 9");
+    System.out.println("=================================\n");
+
+    for (int i = 1; i <= 10; i++) {
+        System.out.println( i + "x" + numMulti + "=" + (i*numMulti));
+    }
+    System.out.println("\n--------------------------------------------------\n");
+    System.out.println("Até que peguei a ideia facil!\n");
+    }
+}
